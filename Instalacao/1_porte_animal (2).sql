e2b4f66782d41194c7148d500592ec75b4e9e7c5
EduardaAlv <eduardasantosalveseduarda@gmail.com>
2023-01-18 18:06:51 -0300

    Update

---
+++ Instalacao/1_porte_animal (2).sql
@@ -0,0 +1,3 @@
+insert into porte_animal (cod_portanimal, descricao) values (1, 'Pequeno');
+insert into porte_animal (cod_portanimal, descricao) values (2, 'Médio');
+insert into porte_animal (cod_portanimal, descricao) values (3, 'Grande');
---
+++ Instalacao/3_clinica (2).sql
@@ -0,0 +1,701 @@
+
+/* INSERT QUERY NO: 1 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+1, 'Klocko-Wiegand', 'Mccormick', 'Dragash'
+);
+
+/* INSERT QUERY NO: 2 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+2, 'Pollich LLC', '6th', 'Calibishie'
+);
+
+/* INSERT QUERY NO: 3 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+3, 'Hane and Sons', 'Sunfield', 'Tayirove'
+);
+
+/* INSERT QUERY NO: 4 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+4, 'Friesen-Stanton', 'Bashford', 'Guojiaba'
+);
+
+/* INSERT QUERY NO: 5 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+5, 'Harris and Sons', 'Arizona', 'Al Khirāb'
+);
+
+/* INSERT QUERY NO: 6 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+6, '"Reilly', ' Beahan and Thompson"', 'American'
+);
+
+/* INSERT QUERY NO: 7 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+7, '"Swift', ' Walsh and Larson"', 'Portage'
+);
+
+/* INSERT QUERY NO: 8 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+8, 'Nienow LLC', 'Cascade', 'Cañas'
+);
+
+/* INSERT QUERY NO: 9 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+9, '"Schowalter', ' Monahan and Streich"', 'Eggendart'
+);
+
+/* INSERT QUERY NO: 10 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+10, 'Donnelly-Swift', 'Shelley', 'Ngrowo'
+);
+
+/* INSERT QUERY NO: 11 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+11, 'Witting-Howe', 'Basil', 'Luhyny'
+);
+
+/* INSERT QUERY NO: 12 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+12, 'Turcotte-Cassin', 'Merry', 'Sebulu'
+);
+
+/* INSERT QUERY NO: 13 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+13, 'O\'Keefe-Witting', 'Hermina', 'Beregovoy'
+);
+
+/* INSERT QUERY NO: 14 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+14, 'Haag-Gleichner', 'Prentice', 'Pitangueiras'
+);
+
+/* INSERT QUERY NO: 15 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+15, 'O\'Hara Inc', 'American', 'Ceranów'
+);
+
+/* INSERT QUERY NO: 16 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+16, '"Bayer', ' Johnson and Koss"', 'Cambridge'
+);
+
+/* INSERT QUERY NO: 17 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+17, '"Tromp', ' Sporer and Bode"', 'School'
+);
+
+/* INSERT QUERY NO: 18 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+18, '"Hermann', ' Green and Emard"', 'Huxley'
+);
+
+/* INSERT QUERY NO: 19 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+19, 'Erdman-Crist', 'Hazelcrest', 'Huazhaizi'
+);
+
+/* INSERT QUERY NO: 20 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+20, 'Daniel and Sons', 'Bartillon', 'Liuxia'
+);
+
+/* INSERT QUERY NO: 21 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+21, 'Feeney Group', 'Declaration', 'Shalushka'
+);
+
+/* INSERT QUERY NO: 22 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+22, '"Konopelski', ' Hahn and Weissnat"', 'Kingsford'
+);
+
+/* INSERT QUERY NO: 23 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+23, '"O\'Hara', ' Metz and Kuhic"', 'Golf Course'
+);
+
+/* INSERT QUERY NO: 24 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+24, 'Fisher Inc', 'Corben', 'Guajará Mirim'
+);
+
+/* INSERT QUERY NO: 25 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+25, 'Ondricka-Buckridge', 'Orin', 'Sechelt'
+);
+
+/* INSERT QUERY NO: 26 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+26, 'Mertz LLC', 'Maple Wood', 'Oskarshamn'
+);
+
+/* INSERT QUERY NO: 27 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+27, 'Lynch-Mann', 'Victoria', 'Ban Na San'
+);
+
+/* INSERT QUERY NO: 28 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+28, '"Bauch', ' Roob and Marks"', 'Eagan'
+);
+
+/* INSERT QUERY NO: 29 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+29, 'Ryan Inc', 'Roxbury', 'San Diego'
+);
+
+/* INSERT QUERY NO: 30 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+30, 'Ankunding LLC', 'Scofield', 'Laotai'
+);
+
+/* INSERT QUERY NO: 31 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+31, 'Johns-Veum', 'Dovetail', 'Salamanca'
+);
+
+/* INSERT QUERY NO: 32 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+32, '"Zulauf', ' Greenfelder and Douglas"', 'New Castle'
+);
+
+/* INSERT QUERY NO: 33 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+33, 'Brown-Schoen', 'Reinke', 'Xiongzhou'
+);
+
+/* INSERT QUERY NO: 34 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+34, 'Hills and Sons', 'Blaine', 'Hāgere Hiywet'
+);
+
+/* INSERT QUERY NO: 35 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+35, 'Heidenreich Group', 'Larry', 'Kamru'
+);
+
+/* INSERT QUERY NO: 36 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+36, 'Homenick-Abshire', 'South', 'Liboran'
+);
+
+/* INSERT QUERY NO: 37 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+37, 'Langosh Group', 'Columbus', 'Calabugao'
+);
+
+/* INSERT QUERY NO: 38 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+38, 'Effertz-Howell', 'Spohn', 'Farīdpur'
+);
+
+/* INSERT QUERY NO: 39 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+39, 'Lang-Walsh', 'Crescent Oaks', 'Bluefields'
+);
+
+/* INSERT QUERY NO: 40 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+40, 'Schuster-Rowe', 'Moulton', 'Redakodi'
+);
+
+/* INSERT QUERY NO: 41 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+41, '"Schneider', ' DuBuque and Bergnaum"', 'Columbus'
+);
+
+/* INSERT QUERY NO: 42 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+42, 'Kemmer-Ondricka', 'Bluejay', 'Kakamega'
+);
+
+/* INSERT QUERY NO: 43 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+43, '"Walter', ' Schiller and Glover"', 'Gateway'
+);
+
+/* INSERT QUERY NO: 44 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+44, 'Hammes LLC', 'Nova', 'Novyy Karachay'
+);
+
+/* INSERT QUERY NO: 45 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+45, 'Gleichner Group', 'Fisk', 'Trunovskoye'
+);
+
+/* INSERT QUERY NO: 46 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+46, '"Schimmel', ' Carroll and O\'Conner"', 'Scoville'
+);
+
+/* INSERT QUERY NO: 47 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+47, 'Hoppe-Botsford', 'Banding', 'Głogówek'
+);
+
+/* INSERT QUERY NO: 48 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+48, 'Little-Roob', 'Victoria', 'Pola'
+);
+
+/* INSERT QUERY NO: 49 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+49, 'Greenholt LLC', 'Killdeer', 'Lapa'
+);
+
+/* INSERT QUERY NO: 50 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+50, '"Ortiz', ' Runolfsdottir and O\'Reilly"', 'Chive'
+);
+
+/* INSERT QUERY NO: 51 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+51, 'Wolf-Baumbach', 'Buhler', 'Panguna'
+);
+
+/* INSERT QUERY NO: 52 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+52, 'Rempel LLC', 'Waxwing', 'Choco'
+);
+
+/* INSERT QUERY NO: 53 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+53, 'Ratke-Wisozk', 'Mariners Cove', 'San José de Colinas'
+);
+
+/* INSERT QUERY NO: 54 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+54, '"Murazik', ' Fahey and Jerde"', 'Gina'
+);
+
+/* INSERT QUERY NO: 55 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+55, 'Dibbert-Schultz', 'Macpherson', 'Olo'
+);
+
+/* INSERT QUERY NO: 56 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+56, '"Sipes', ' Frami and Breitenberg"', 'Ridgeway'
+);
+
+/* INSERT QUERY NO: 57 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+57, 'Strosin-Kilback', 'Dapin', 'Manggissari'
+);
+
+/* INSERT QUERY NO: 58 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+58, 'Gibson Inc', 'Clemons', 'Baochang'
+);
+
+/* INSERT QUERY NO: 59 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+59, 'Gusikowski-Welch', 'Acker', 'Dengzishan'
+);
+
+/* INSERT QUERY NO: 60 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+60, '"Halvorson', ' Berge and Daugherty"', 'School'
+);
+
+/* INSERT QUERY NO: 61 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+61, '"Schimmel', ' D\'Amore and King"', 'Service'
+);
+
+/* INSERT QUERY NO: 62 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+62, '"Zulauf', ' Schumm and Weber"', 'Becker'
+);
+
+/* INSERT QUERY NO: 63 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+63, 'Thiel Inc', 'Green', 'Jagdaqi'
+);
+
+/* INSERT QUERY NO: 64 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+64, '"Wunsch', ' Bogan and Rodriguez"', 'Maple'
+);
+
+/* INSERT QUERY NO: 65 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+65, '"Abshire', ' Cartwright and Ebert"', 'Scott'
+);
+
+/* INSERT QUERY NO: 66 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+66, 'Dach Group', 'Miller', 'Saint-Ouen'
+);
+
+/* INSERT QUERY NO: 67 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+67, 'Conroy-Kulas', 'Warner', 'Kushk'
+);
+
+/* INSERT QUERY NO: 68 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+68, 'Spencer-Murray', 'Ohio', 'Etinan'
+);
+
+/* INSERT QUERY NO: 69 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+69, 'Rolfson-Lockman', 'Dexter', 'Itajaí'
+);
+
+/* INSERT QUERY NO: 70 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+70, 'Dickinson-Rippin', 'Algoma', 'Tangtu'
+);
+
+/* INSERT QUERY NO: 71 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+71, 'Pollich-Kuphal', 'Old Shore', 'Chaiyaphum'
+);
+
+/* INSERT QUERY NO: 72 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+72, 'Schumm and Sons', 'Golf', 'Pescara'
+);
+
+/* INSERT QUERY NO: 73 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+73, 'Kuvalis Inc', 'Laurel', 'Avelinha'
+);
+
+/* INSERT QUERY NO: 74 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+74, 'Tromp-Altenwerth', 'Mendota', 'Citeguh'
+);
+
+/* INSERT QUERY NO: 75 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+75, 'Kuhlman-Walsh', 'Namekagon', 'Paraparaumu'
+);
+
+/* INSERT QUERY NO: 76 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+76, 'Conn-Kris', 'Beilfuss', 'Severnyy'
+);
+
+/* INSERT QUERY NO: 77 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+77, 'Pfeffer and Sons', 'Pennsylvania', 'Kitāf'
+);
+
+/* INSERT QUERY NO: 78 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+78, '"Boyle', ' Kassulke and King"', 'Hoepker'
+);
+
+/* INSERT QUERY NO: 79 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+79, 'Miller-Reynolds', 'Melvin', 'Huangzhuang'
+);
+
+/* INSERT QUERY NO: 80 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+80, 'Skiles-Conroy', 'Lotheville', 'Uthai Thani'
+);
+
+/* INSERT QUERY NO: 81 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+81, 'Heidenreich-Lowe', 'Leroy', 'Karanyowka'
+);
+
+/* INSERT QUERY NO: 82 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+82, '"Witting', ' McKenzie and Mosciski"', 'Sherman'
+);
+
+/* INSERT QUERY NO: 83 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+83, 'Kovacek-Olson', 'Buhler', 'Nice'
+);
+
+/* INSERT QUERY NO: 84 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+84, '"Hudson', ' Durgan and Watsica"', 'Declaration'
+);
+
+/* INSERT QUERY NO: 85 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+85, '"Hamill', ' Mosciski and Will"', 'Delaware'
+);
+
+/* INSERT QUERY NO: 86 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+86, 'Abshire-Oberbrunner', 'Thompson', 'Al Qurayn'
+);
+
+/* INSERT QUERY NO: 87 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+87, '"Kuhic', ' Stark and Larkin"', 'Sutteridge'
+);
+
+/* INSERT QUERY NO: 88 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+88, 'Waters-Veum', 'Steensland', 'Kristiansund N'
+);
+
+/* INSERT QUERY NO: 89 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+89, 'O\'Keefe Inc', 'Fisk', 'Göteborg'
+);
+
+/* INSERT QUERY NO: 90 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+90, 'Kuvalis Group', 'Donald', 'Huifa'
+);
+
+/* INSERT QUERY NO: 91 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+91, 'Crooks Inc', 'Stephen', 'Firenze'
+);
+
+/* INSERT QUERY NO: 92 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+92, 'Kuphal Inc', 'Evergreen', 'Dungon'
+);
+
+/* INSERT QUERY NO: 93 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+93, 'Simonis and Sons', 'Hansons', 'Xin’an'
+);
+
+/* INSERT QUERY NO: 94 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+94, 'Schultz-Gleichner', 'Cardinal', 'Intipucá'
+);
+
+/* INSERT QUERY NO: 95 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+95, '"Walter', ' O\'Reilly and Vandervort"', 'Pankratz'
+);
+
+/* INSERT QUERY NO: 96 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+96, 'Grady-Lesch', 'Monica', 'Armenia'
+);
+
+/* INSERT QUERY NO: 97 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+97, 'Schaden and Sons', 'Muir', 'Novyy Starodub'
+);
+
+/* INSERT QUERY NO: 98 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+98, 'Ward and Sons', 'Stuart', 'Kobylanka'
+);
+
+/* INSERT QUERY NO: 99 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+99, 'Goodwin Inc', 'Debra', 'Grecheskoye'
+);
+
+/* INSERT QUERY NO: 100 */
+INSERT INTO clinica(cod_clinica, bairro, rua, cidade)
+VALUES
+(
+100, 'Heathcote Group', 'Sachs', 'Jaguaquara'
+);
+
